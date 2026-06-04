04466004aecdef5b7e13fcf8cbc806bc9a26fe93
Luís Guilherme <luis.bl.guilherme@gmail.com>
2026-06-04 19:13:46 -0300

    function_espacopelotempo.c

--- function_espacopelotempo.c
+++ function_espacopelotempo.c
@@ -1,14 +1,4 @@
-#ifndef FUNCTION_ESPACOPELOTEMPO_C
-#define FUNCTION_ESPACOPELOTEMPO_C
-
-// S = v * t  (sem aceleracao)
-double calcularEspaco(double velocidade, double tempo) {
-    return velocidade * tempo;
+float espacopelotempo(float velocidade, float tempo, float aceleracao) 
+{
+    return (velocidade * tempo) + ((aceleracao * tempo * tempo) / 2);
 }
-
-// S = (v * t) + (a * t^2) / 2  (com aceleracao constante)
-double calcularEspacoComAceleracao(double velocidade, double tempo, double aceleracao) {
-    return (velocidade * tempo) + (aceleracao * tempo * tempo) / 2.0;
-}
-
-#endif
